3fd3aea2c8e17503f2fc311b4fb14c4f6a213768
bdillon1 <benjamin.dillon@valpo.edu>
2017-05-11 21:16:08 -0500

    FINAL Update of checkpower.sh

--- checkPower.sh
+++ checkPower.sh
@@ -12,7 +12,7 @@ do
     echo "cannot connect to remote host"
     powerLoss=$(($powerLoss + 1))
     if [[ $emailSent -lt 3 && $powerLoss -gt 5 ]]; then
-      echo "Sending email to $EMAIL"
+      echo "Sending email to $EMAIL" 
       echo "Power outage detected. `date`" | mail -s "ALERT: Power loss" -a "From: alert@fromblah.com" $EMAIL
       emailSent=$(($emailSent + 1))
     fi
